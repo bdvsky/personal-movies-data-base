c28aa3657abce5d348658abbfba292db37d33a46
Ihor <example@gmail.com>
2022-05-16 11:58:02 +0300

    Second commit

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ const personalMoviesDB = {
     genres: [],
     privat: false
 };
-const lastViewed = prompt('Один из последних просмотренных фильмов?', '');
-const rate = +prompt('На сколько оцените его?', '');
-personalMoviesDB.movies[lastViewed] = rate;
+
+if(personalMoviesDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if(personalMoviesDB.count < 30) {
+    alert('Вы классический зритель');
+} else if(personalMoviesDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''); 
+    const b = +prompt('На сколько оцените его?', '');
+    
+    if(a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+        personalMoviesDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('error');
+    }
+}
 console.log(personalMoviesDB);
